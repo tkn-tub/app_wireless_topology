6816b30915ba649e01dc6d3b402e3c1e0051c6f3
zubow <zubow@tkn.tu-berlin.de>
2016-02-26 12:48:30 +0100

    renaming functions

--- wishful_module_wireless_topology/wireless_topology_module.py
+++ wishful_module_wireless_topology/wireless_topology_module.py
@@ -96,7 +96,7 @@ class WirelessTopologyModule(wishful_controller.ControllerUpiModule):
 
         try:
             self.log.debug('(2) single flow at %s' % str(node1))
-            rv = self.controller.nodes(node1).blocking(True).net.gen_backlogged_layer2_traffic(mon_dev, 1000, 1350, 12, ipdst="1.1.1.1", ipsrc="2.2.2.2", use_tcpreplay=True)
+            rv = self.controller.nodes(node1).blocking(True).net.gen_backlogged_layer2_traffic(mon_dev, 1000, 12, ipPayloadSize=1350, ipdst="1.1.1.1", ipsrc="2.2.2.2", use_tcpreplay=True)
 
             peer_node = node1
             single_tx_rate_stats[peer_node] = rv
@@ -104,7 +104,7 @@ class WirelessTopologyModule(wishful_controller.ControllerUpiModule):
             time.sleep(1)
 
             self.log.debug('(3) single flow at %s' % str(node2))
-            rv = self.controller.nodes(node2).blocking(True).net.gen_backlogged_layer2_traffic(mon_dev, 1000, 1350, 12, ipdst="1.1.1.1", ipsrc="2.2.2.2", use_tcpreplay=True)
+            rv = self.controller.nodes(node2).blocking(True).net.gen_backlogged_layer2_traffic(mon_dev, 1000, 12, ipPayloadSize=1350, ipdst="1.1.1.1", ipsrc="2.2.2.2", use_tcpreplay=True)
 
             peer_node = node2
             single_tx_rate_stats[peer_node] = rv
